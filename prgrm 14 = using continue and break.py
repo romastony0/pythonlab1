3103ed103b86ed2586a2bd9c4705870f2ea1be94
Romas Tony P <94161293+romastony0@users.noreply.github.com>
2022-06-07 12:47:42 +0530

    Update prgrm 14 = using continue and break.py

--- prgrm 14 = using continue and break.py
+++ prgrm 14 = using continue and break.py
@@ -1,14 +1,15 @@
 import random;
 
 r = random.randint(1,100);
+print("THE RANDOM NUMBER IS:",r)
 
-while(r):
-    if r%2==0:
+
+if r%2==0 and r<=49:
       print("The random number is even and the number is:",r);
-
-    elif(r>=50 and r<=59):
+while(r):
+    if(r>=50 and r<=59):
         print("The number is between 50-59 and the number is:",r)
-        continue;
+        continue
     elif (r >= 96 and r <= 99):
         print("The number is between 96-99 and the number is:", r)
-    break
+        break
